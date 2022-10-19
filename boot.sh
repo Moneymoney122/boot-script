ae87f8250b91c68d04eb6aae77f6edf51ecce6dc
James Chandler <jameschandler750@gmail.com>
2022-10-19 22:46:32 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -107,10 +107,24 @@ exit ;;
 OwO) echo "OwO" && exit ;;
 ssh)
 echo -e "${ICyan}ssh into a root shell for your device, your device must be jailbroken and have OpenSSH installed.\nYour device and computer must be on the same WiFi network,\nand there must not be anything stopping your devices from connecting to each other.\nThe default password for your device if you have not changed it is: alpine.\nEnter your device's local IP Address.\nYou can find this in Settings > WiFi > Click on your WiFi Network and scroll down and find your local IP Address.\nWhen you have finished in the shell type \"exit\" or \"logout\".\nRemember, you are logging in as root, the most powerfull user and it can be dangerous if you don't know what you're doing.\nProcceed at your own risk, I am not responsible for any damage you may cause. To exit at anytime press Ctrl+C.\nWhen entering your password it will be hidden but will still be registered.\n"
+ echo "Checking Internet connection..." 
+    ping -c 3 208.67.222.222 >/dev/null 2>/dev/null
+    if [[ $? != 0 ]]; then
+   
+   echo "No internet connection."
+   
+   echo -e "\nYou might encounter issues"
+   
+    else
+
+       echo "You have an internet connection."
+    
+   fi
+
 read -p "Enter local IP Address: " IPADDRESS
 read -sp "Enter Password: " PASSWORD
 echo -e "${NC}"
-sudo sshpass -p "$PASSWORD" ssh root@$IPADDRESS
+sshpass -p "$PASSWORD" ssh root@$IPADDRESS
 exit ;;
 esac
 
