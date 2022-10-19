aacabee0feda42a085238e363d3b846f1742b3eb
James Chandler <jameschandler750@gmail.com>
2022-10-19 22:50:22 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -113,11 +113,11 @@ echo -e "${ICyan}ssh into a root shell for your device, your device must be jail
    
    echo "No internet connection."
    
-   echo -e "\nYou might encounter issues"
+   echo -e "You might encounter issues sshing into your device\n"
    
     else
 
-       echo "You have an internet connection."
+       echo -e "You have an internet connection."
     
    fi
 
