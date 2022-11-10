101283bd7e974d136183ce36186c7a81bd30cb0b
James Chandler <jameschandler750@gmail.com>
2022-11-10 12:23:19 +0000

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -13,6 +13,8 @@
 
 #start of script
 
+#yes i know this code is bad, but it mostly works
+
 #!/usr/bin/env bash
 
 #colours
@@ -90,7 +92,7 @@ fi
 case $1 in 
 OwO) echo -e "${ICyan}OwO${NC}" && exit ;;
 ssh)
-echo -e "${ICyan} $line_length"
+echo -e "${ICyan}${line_length}"
 echo -e "ssh into a root shell for your device, your device must be jailbroken and have OpenSSH installed.\nYour device and computer must be on the same WiFi network,\nand there must not be anything stopping your devices from connecting to each other.\nThe default password for your device if you have not changed it is: alpine.\nEnter your device's local IP Address.\nYou can find this in Settings > WiFi > Click on your WiFi Network and scroll down and find your local IP Address.\nWhen you have finished in the shell type \"exit\" or \"logout\".\nRemember, you are logging in as root, the most powerfull user and it can be dangerous if you don't know what you're doing.\nProcceed at your own risk, I am not responsible for any damage you may cause. To exit at anytime press Ctrl+C.\nWhen entering your password it will be hidden but will still be registered."
 echo "$line_length"
 echo "Checking Internet connection..." 
